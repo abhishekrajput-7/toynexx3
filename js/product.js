6b42b26fd3180d55c218a3b0c22a515b4a0f6ada
abhishekrajput-7 <abhishek282501@gmail.com>
2026-07-26 01:25:28 +0530

    Add files via upload

--- js/product.js
+++ js/product.js
@@ -1,8 +1,35 @@
-// Get Product ID from URL
-
 const params = new URLSearchParams(window.location.search);
-
 const productId = params.get("id");
 
-console.log(productId);
+const product = PRODUCTS.find(p => p.id === productId);
 
+if (!product) {
+    document.getElementById("productName").textContent = "Product Not Found";
+    throw new Error("Product not found");
+}
+
+document.getElementById("productName").textContent = product.name;
+document.getElementById("productPrice").textContent = `₹${product.price}`;
+document.getElementById("productDescription").textContent = product.desc;
+
+document.getElementById("mainProductImage").src = product.image;
+
+const thumbs = document.querySelector(".thumbnail-gallery");
+
+thumbs.innerHTML = "";
+
+product.gallery.forEach(img => {
+
+    const image = document.createElement("img");
+
+    image.src = img;
+
+    image.className = "thumb";
+
+    image.onclick = () => {
+        document.getElementById("mainProductImage").src = img;
+    };
+
+    thumbs.appendChild(image);
+
+});
